8a1bb2386fe5bba774c8d414b1a534ec53316e74
Mazi intel <matthvnc@yahoo.com>
2022-07-09 15:49:24 -0700

    task0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,9 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
 
 int _putchar(char c);
