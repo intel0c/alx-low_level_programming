80dd3091671f671b11e85af4658afc7156dd639c
Mazi intel <matthvnc@yahoo.com>
2022-07-12 12:48:51 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This Project is for pointers, strings, arrays in c programming
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates the vale it points to 98
+ * @n: pointer to int n
+ */
+
+void reset_to_98(int *n);
