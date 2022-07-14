b9a926ef75ff977a894c2ee93789d00d5b969257
Mazi intel <matthvnc@yahoo.com>
2022-07-13 19:23:54 -0700

    correction task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -20,9 +20,10 @@ int _strlen(char *s)
 }
 
 /**
- * puts - prints every other character of a string, starting with the first character, followed by a new line.
+ * puts2 - prints every second character of a string, followed by a new line
  * @str: string to print
  */
+
 void puts2(char *str)
 {
 	int i;
